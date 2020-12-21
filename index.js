9f2561b1f800863d47a2ed43993a3dea6f8ed1f1
Jason J Cruz <jaycru06@yahoo.com>
2020-12-21 08:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const map = (sourceArray, callback) => {
 
 const reduce = (sourceArray, callback, startingVal = 0) => {
     let storedVal = startingVal
+<<<<<<< HEAD
     
     if (!sourceArray.every(Number)) {
       storedVal = true 
@@ -19,4 +20,34 @@ const reduce = (sourceArray, callback, startingVal = 0) => {
       storedVal = callback(value, storedVal)
     } 
     return storedVal
+=======
+    if (!sourceArray.every(Number)) {
+      storedVal = true 
+      for (let value of sourceArray) {
+        storedVal = callback(value, storedVal)
+      }
+    } else {
+        for (let value of sourceArray) {
+          storedVal = callback(value, storedVal)
+        } 
+    }
+    return storedVal
+    // let result
+    // let total = 0
+    // if (sourceArray.every(Number)) {
+    //   sourceArray.forEach(value => {
+    //     total += value 
+    //   });
+    // } else {
+    //   sourceArray.forEach(value => {
+    //     if (!value) {
+    //       total = false
+    //     } else {
+    //       total = true
+    //     }
+    //   });
+    // }
+    // result = callback(total, startingVal)  
+    // return result
+>>>>>>> f486e8286ea75bf5a88ac5289c3e3a040774dd04
 }
